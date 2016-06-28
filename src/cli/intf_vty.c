OpenSwitchNOS/openswitch-ops-intfd

ops-utils call for setting and getting tag and trunked VLANs for port table
Tags: fix, dev
TG-1175

Change-Id: Iad4ea9faf8a8b25944cb157359b0ed8871b9ae06
Signed-off-by: Harshvardhan <harshvardhan@hpe.com>

src/cli/intf_vty.c
@@ -1266,43 +1266,43 @@ parse_vlan(const char *if_name, struct vty* vty)
     else if (strcmp(port_row->vlan_mode, OVSREC_PORT_VLAN_MODE_ACCESS) == 0)
     {
         vty_out(vty, "%3s%s%ld%s", "", "vlan access ",
-                *port_row->tag, VTY_NEWLINE);
+                (int64_t)ops_port_get_tag(port_row, idl), VTY_NEWLINE);
     }
     else if (strcmp(port_row->vlan_mode, OVSREC_PORT_VLAN_MODE_TRUNK) == 0)
     {
-        for (i = 0; i < port_row->n_trunks; i++)
+        for (i = 0; i < port_row->n_vlan_trunks; i++)
         {
             vty_out(vty, "%3s%s%ld%s", "", "vlan trunk allowed ",
-                    port_row->trunks[i], VTY_NEWLINE);
+                    (int64_t)ops_port_get_trunks(port_row, i, idl), VTY_NEWLINE);
         }
     }
     else if (strcmp(port_row->vlan_mode,
             OVSREC_PORT_VLAN_MODE_NATIVE_UNTAGGED) == 0)
     {
-        if (port_row->n_tag == 1)
+        if (port_row->vlan_tag != NULL)
         {
             vty_out(vty, "%3s%s%ld%s", "", "vlan trunk native ",
-                    *port_row->tag, VTY_NEWLINE);
+                    (int64_t)ops_port_get_tag(port_row, idl), VTY_NEWLINE);
         }
-        for (i = 0; i < port_row->n_trunks; i++)
+        for (i = 0; i < port_row->n_vlan_trunks; i++)
         {
             vty_out(vty, "%3s%s%ld%s", "", "vlan trunk allowed ",
-                    port_row->trunks[i], VTY_NEWLINE);
+                    (int64_t)ops_port_get_trunks(port_row, i, idl), VTY_NEWLINE);
         }
     }
     else if (strcmp(port_row->vlan_mode,
             OVSREC_PORT_VLAN_MODE_NATIVE_TAGGED) == 0)
     {
-        if (port_row->n_tag == 1)
+        if (port_row->vlan_tag != NULL)
         {
             vty_out(vty, "%3s%s%ld%s", "", "vlan trunk native ",
-                    *port_row->tag, VTY_NEWLINE);
+                    (int64_t)ops_port_get_tag(port_row, idl), VTY_NEWLINE);
         }
         vty_out(vty, "%3s%s%s", "", "vlan trunk native tag",VTY_NEWLINE);
-        for (i = 0; i < port_row->n_trunks; i++)
+        for (i = 0; i < port_row->n_vlan_trunks; i++)
         {
             vty_out(vty, "%3s%s%ld%s", "", "vlan trunk allowed ",
-                    port_row->trunks[i], VTY_NEWLINE);
+                    (int64_t)ops_port_get_trunks(port_row, i, idl), VTY_NEWLINE);
         }
     }
 
@@ -2313,8 +2313,8 @@ show_lacp_interfaces_brief (struct vty *vty, const char *argv[])
 
         vty_out(vty, " %-15s ", lag_port->name);
         /* Display vid for an lag interface */
-        if (lag_port->tag != NULL ) {
-            vty_out(vty, "%-8ld", *lag_port->tag); /*vid */
+        if (lag_port->vlan_tag != NULL ) {
+            vty_out(vty, "%-8ld", (int64_t)ops_port_get_tag(lag_port, idl)); /*vid */
         }
         else {
             vty_out(vty, "--      "); /*vid */
@@ -2707,20 +2707,20 @@ show_interface_status(struct vty *vty, const const struct ovsrec_interface *ifro
          * entered into interface mode
          */
         if (port_row == NULL ||
-            (port_row->tag == NULL && port_row->vlan_mode == NULL)) {
+            (port_row->vlan_tag == NULL && port_row->vlan_mode == NULL)) {
             vty_out(vty, "--      "); /*vid */
             vty_out(vty, "eth  "); /*type */
             vty_out(vty, "%-7s", VLAN_MODE_ROUTED);
         }
         /* Display vlan mode and vid for interface VLAN*/
-        else if (port_row->tag != NULL && port_row->vlan_mode == NULL) {
+        else if (port_row->vlan_tag != NULL && port_row->vlan_mode == NULL) {
             vty_out(vty, "--      "); /*vid */
             vty_out(vty, "eth  "); /*type */
             vty_out(vty, "       "); /* mode - routed or not */
         }
         /* Display vlan mode and vid for an l2 interface */
-        else if (port_row->tag != NULL && port_row->vlan_mode != NULL) {
-            vty_out(vty, "%-8ld", *port_row->tag); /*vid */
+        else if (port_row->vlan_tag != NULL && port_row->vlan_mode != NULL) {
+            vty_out(vty, "%-8ld", (int64_t)ops_port_get_tag(port_row, idl)); /*vid */
             vty_out(vty, "eth  "); /*type */
             if (strncmp(port_row->vlan_mode, OVSREC_PORT_VLAN_MODE_ACCESS,
                         strlen(OVSREC_PORT_VLAN_MODE_ACCESS)) == 0){

vgribov/ops-intfd

src/cli/intf_vty.c
@@ -1266,43 +1266,43 @@ parse_vlan(const char *if_name, struct vty* vty)
     else if (strcmp(port_row->vlan_mode, OVSREC_PORT_VLAN_MODE_ACCESS) == 0)
     {
         vty_out(vty, "%3s%s%ld%s", "", "vlan access ",
-                *port_row->tag, VTY_NEWLINE);
+                (int64_t)ops_port_get_tag(port_row), VTY_NEWLINE);
     }
     else if (strcmp(port_row->vlan_mode, OVSREC_PORT_VLAN_MODE_TRUNK) == 0)
     {
-        for (i = 0; i < port_row->n_trunks; i++)
+        for (i = 0; i < port_row->n_vlan_trunks; i++)
         {
             vty_out(vty, "%3s%s%ld%s", "", "vlan trunk allowed ",
-                    port_row->trunks[i], VTY_NEWLINE);
+                    (int64_t)ops_port_get_trunks(port_row, i), VTY_NEWLINE);
         }
     }
     else if (strcmp(port_row->vlan_mode,
             OVSREC_PORT_VLAN_MODE_NATIVE_UNTAGGED) == 0)
     {
-        if (port_row->n_tag == 1)
+        if (port_row->vlan_tag != NULL)
         {
             vty_out(vty, "%3s%s%ld%s", "", "vlan trunk native ",
-                    *port_row->tag, VTY_NEWLINE);
+                    (int64_t)ops_port_get_tag(port_row), VTY_NEWLINE);
         }
-        for (i = 0; i < port_row->n_trunks; i++)
+        for (i = 0; i < port_row->n_vlan_trunks; i++)
         {
             vty_out(vty, "%3s%s%ld%s", "", "vlan trunk allowed ",
-                    port_row->trunks[i], VTY_NEWLINE);
+                    (int64_t)ops_port_get_trunks(port_row, i), VTY_NEWLINE);
         }
     }
     else if (strcmp(port_row->vlan_mode,
             OVSREC_PORT_VLAN_MODE_NATIVE_TAGGED) == 0)
     {
-        if (port_row->n_tag == 1)
+        if (port_row->vlan_tag != NULL)
         {
             vty_out(vty, "%3s%s%ld%s", "", "vlan trunk native ",
-                    *port_row->tag, VTY_NEWLINE);
+                    (int64_t)ops_port_get_tag(port_row), VTY_NEWLINE);
         }
         vty_out(vty, "%3s%s%s", "", "vlan trunk native tag",VTY_NEWLINE);
-        for (i = 0; i < port_row->n_trunks; i++)
+        for (i = 0; i < port_row->n_vlan_trunks; i++)
         {
             vty_out(vty, "%3s%s%ld%s", "", "vlan trunk allowed ",
-                    port_row->trunks[i], VTY_NEWLINE);
+                    (int64_t)ops_port_get_trunks(port_row, i), VTY_NEWLINE);
         }
     }
 
@@ -2313,8 +2313,8 @@ show_lacp_interfaces_brief (struct vty *vty, const char *argv[])
 
         vty_out(vty, " %-15s ", lag_port->name);
         /* Display vid for an lag interface */
-        if (lag_port->tag != NULL ) {
-            vty_out(vty, "%-8ld", *lag_port->tag); /*vid */
+        if (lag_port->vlan_tag != NULL ) {
+            vty_out(vty, "%-8ld", (int64_t)ops_port_get_tag(lag_port)); /*vid */
         }
         else {
             vty_out(vty, "--      "); /*vid */
@@ -2707,20 +2707,20 @@ show_interface_status(struct vty *vty, const const struct ovsrec_interface *ifro
          * entered into interface mode
          */
         if (port_row == NULL ||
-            (port_row->tag == NULL && port_row->vlan_mode == NULL)) {
+            (port_row->vlan_tag == NULL && port_row->vlan_mode == NULL)) {
             vty_out(vty, "--      "); /*vid */
             vty_out(vty, "eth  "); /*type */
             vty_out(vty, "%-7s", VLAN_MODE_ROUTED);
         }
         /* Display vlan mode and vid for interface VLAN*/
-        else if (port_row->tag != NULL && port_row->vlan_mode == NULL) {
+        else if (port_row->vlan_tag != NULL && port_row->vlan_mode == NULL) {
             vty_out(vty, "--      "); /*vid */
             vty_out(vty, "eth  "); /*type */
             vty_out(vty, "       "); /* mode - routed or not */
         }
         /* Display vlan mode and vid for an l2 interface */
-        else if (port_row->tag != NULL && port_row->vlan_mode != NULL) {
-            vty_out(vty, "%-8ld", *port_row->tag); /*vid */
+        else if (port_row->vlan_tag != NULL && port_row->vlan_mode != NULL) {
+            vty_out(vty, "%-8ld", (int64_t)ops_port_get_tag(port_row)); /*vid */
             vty_out(vty, "eth  "); /*type */
             if (strncmp(port_row->vlan_mode, OVSREC_PORT_VLAN_MODE_ACCESS,
                         strlen(OVSREC_PORT_VLAN_MODE_ACCESS)) == 0){