460be7563d0552fa6145c1364f24f12d6e649ef8
Yash Parulekar <yash.parulekar@hpe.com>
2016-07-05 17:11:00 -0700

    bridge_normal cannot be deleted
    TG-1433
    
    Change-Id: I4aa25294176926007e485837d50cad68736c787b
    Signed-off-by: Yash Parulekar <yash.parulekar@hpe.com>

--- opsplugins/interface.py
+++ opsplugins/interface.py
@@ -9,6 +9,10 @@ class InterfaceValidator(BaseValidator):
     def validate_deletion(self, validation_args):
         intf_row = validation_args.resource_row
         intf_type = get_column_data_from_row(intf_row, "type")
+        intf_name = get_column_data_from_row(intf_row, "name")
         if intf_type == 'system':
             details = "Physical interfaces cannot be deleted"
+            raise ValidationError(error.VERIFICATION_FAILED, details)
+        if intf_type == 'internal' and intf_name == 'bridge_normal':
+            details = "Bridge_normal interface cannot be deleted"
             raise ValidationError(error.VERIFICATION_FAILED, details)
